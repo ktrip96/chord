a36601f096a262e1892610ac5ad5fe23d011e37c
gnostis <saridakisbitos@gmail.com>
2021-03-13 23:37:52 +0200

    No! No! I'm not an automated comment! It's all lies!

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-
 // Required
 const express = require('express')
 const socketio = require('socket.io')
@@ -6,8 +5,7 @@ const http = require('http')
 const client_io = require('socket.io-client')
 const sha1 = require('sha1')
 
-const app = express()
-const server = http.createServer(app)
+const server = http.createServer(express())
 const io = socketio(server, {
   cors: {
     origin: '*',
@@ -16,50 +14,97 @@ const io = socketio(server, {
 
 // Juice
 const IP_PORT = process.argv[2]
-const HASH = sha1(IP_PORT)
+const BOOTSTRAP = 'localhost:3000'
 
 const separator = IP_PORT.indexOf(':')
 const IP = IP_PORT.slice(0,separator)
 const PORT = IP_PORT.slice(separator+1)
+const MY_HASH = sha1(IP_PORT)
 
-const BOOTSTRAP ='localhost:3000'
-
-// το παρακάτω γίνεται fireup, όταν στον client
-// εκτελείται η γραμμή let socket = io(localhost:PORT)
-
-if (PORT == 3000) {
+if (IP_PORT == BOOTSTRAP) {
   // Bootstrap
 
-  let next = BOOTSTRAP
   let previous = BOOTSTRAP
+  let previous_hash = MY_HASH
+  let next = BOOTSTRAP
+  let next_hash = MY_HASH
 
   io.on('connection', (socket) => {
+    // On Connection
 
-    // Call Bootstrap Join
-    socket.on('join', ({IP_PORT}) => {
+    socket.on('join', ({join_ip_port}) => {
+      // On Join
+
       if (next == BOOTSTRAP) {
-        previous = IP_PORT
-        next = IP_PORT
-        console.log('Join In Bootstrap:', { previous, next })
-        socket.emit('join_response', { previous: BOOTSTRAP, next: BOOTSTRAP })
+        // Special case: Only BOOTSTRAP in the network
+
+        // 1st joiner next/previous
+        socket = client_io.connect('http://' + join_ip_port)
+        socket.emit('join_response', { join_previous: BOOTSTRAP, join_next: BOOTSTRAP })
+
+        // Bootstrap next/previous
+        let hash = sha1(join_ip_port)
+        previous = join_ip_port
+        previous_hash = hash
+        next = join_ip_port
+        next_hash = hash
+        console.log('1st Join In Bootstrap:', { previous, next })
+
+      } else {
+        join_general_case(join_ip_port, previous_hash, next_hash, previous, next);
       }
-
     })
 
-    socket.on('disconnect', () => {
-      console.log('Disconnected')
+    socket.on('join_update_previous', ({ new_previous }) => {
+      previous = new_previous
+      console.log('New Previous:' + previous)
     })
 
+    socket.on('join_update_next', ({ new_next }) => {
+      next = new_next
+      console.log('New Next:' + next)
+    })
   })
 } else {
   // Non Bootstrap
 
-  let socket = client_io.connect('http://' + BOOTSTRAP)
+  let previous = null
+  let previous_hash = null
+  let next = null
+  let next_hash = null
 
-  socket.emit('join', { IP_PORT })
+  let bootstrap_socket = client_io.connect('http://' + BOOTSTRAP)
 
-  socket.on('join_response', ( {previous,next} ) => {
-      console.log('Join Response Object:', {previous,next})
+  bootstrap_socket.emit('join', { join_ip_port:IP_PORT })
+
+  io.on('connection', (socket) => {
+    // On Connection
+
+    socket.on('join_response', ({ join_previous, join_next }) => {
+      // On Join Response
+
+      console.log('Join Response:', { join_previous, join_next })
+      previous = join_previous
+      next = join_next
+      bootstrap_socket.close()
+    })
+
+    socket.on('join_forward', ({ join_ip_port }) => {
+      // On Join Forward
+
+      console.log('They sent me this guy:', join_ip_port)
+      join_general_case(join_ip_port, previous_hash, next_hash, previous, next);
+    })
+
+    socket.on('join_update_previous', ({ new_previous }) => {
+      previous = new_previous
+      console.log('New Previous:' + previous)
+    })
+
+    socket.on('join_update_next', ({ new_next }) => {
+      next = new_next
+      console.log('New Next:' + next)
+    })
   })
 
 }
@@ -76,3 +121,47 @@ server.listen(PORT, () => {
     - Κρατάμε ένα array που συνδέει PORTS με objects, κάνουμε import το array
     και ο κάθε κόμβος έχει πρόσβαση στο object, με array[PORT].previous π.χ
 */
+
+function join_general_case(join_ip_port, previous_hash, next_hash, previous, next) {
+
+  let hash = sha1(join_ip_port)
+
+    if (hash < MY_HASH) {
+      if (hash < previous_hash) {
+        // Send him to previous
+
+      socket = client_io.connect('http://' + previous)
+      socket.emit('join_forward', { join_ip_port })
+    } else {
+      // Put him between me and previous
+
+      socket = client_io.connect('http://' + join_ip_port)
+      socket.emit('join_response', { join_previous: previous, join_next: IP_PORT })
+
+      socket = client_io.connect('http://' + previous)
+      socket.emit('join_update_next', { new_next: join_ip_port })
+
+      previous = join_ip_port
+      console.log('New previous:' + previous)
+    }
+  } else {
+    if (hash > next_hash) {
+      // Send him to next
+
+      socket = client_io.connect('http://' + next)
+      socket.emit('join_forward', { join_ip_port })
+    } else {
+      // Put him between me and next
+
+      socket = client_io.connect('http://' + join_ip_port)
+      socket.emit('join_response', { join_previous: IP_PORT, join_next: next })
+
+      socket = client_io.connect('http://' + next)
+      socket.emit('join_update_previous', { new_previous: join_ip_port })
+
+      next = join_ip_port
+      console.log('New next:' + next)
+    }
+  } 
+
+}
