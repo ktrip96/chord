6af473dc929af7ca03f86547a749d6fef60b927f
ktrip96 <ktrip96@gmail.com>
2021-03-15 05:51:03 +0200

    Rendering Nodes at screen

--- client/src/components/Chord.js
+++ client/src/components/Chord.js
@@ -4,6 +4,7 @@ import io from 'socket.io-client'
 export default function Chord() {
   // eslint-disable-next-line
   const [serverPort, setServerPort] = useState(5000)
+  const [portArray, setPortArray] = useState([{ ip: 'localhost:5000' }])
 
   useEffect(() => {
     let socket = io(`http://localhost:${serverPort}`)
@@ -15,7 +16,11 @@ export default function Chord() {
 
     socket.on('FRONT_join', ({ join_ip_port }) => {
       // TODO
+      // θέλει προσοχή, γιατί αν είμαι στον κόμβο 5001, το socket.on αφορά το socket
+      // του 5002, και έχει δεν ακούω τον boostrap
+      // Πρέπει κάπου να κρατήσω
       console.log(`${join_ip_port} joined the chord`)
+      setPortArray([...portArray, { ip: join_ip_port }])
       // κάνε update το array που κρατάει τα nodes που είναι αποθηκευμένα στο chord
     })
 
@@ -26,9 +31,32 @@ export default function Chord() {
     // })
   }, [serverPort])
 
+  const chordRender = portArray.map(({ ip }) => <h1>{ip}</h1>)
+  console.log('State is:', portArray)
   return (
     <div>
       <p>Welcome to the Chord</p>
+      {chordRender}
     </div>
   )
 }
+
+/**
+ * console.log('JOIN')
+ *  //* Κώδικας Front
+    socket.on('FRONT_initialise', () => {
+      socket.emit('FRONT_send_data', { ME })
+      front_socket = socket
+    })
+
+     // Kάθε φορά που κάνει κάποιος join στον Bootstrap
+      // στείλε στο Front το αντίστοιχο event.
+      front_socket && front_socket.emit('FRONT_join', { join_ip_port })
+
+
+    //* Κώδικας Front
+    socket.on('FRONT_initialise', () => {
+      socket.emit('FRONT_send_data', { ME })
+    })
+
+ */
