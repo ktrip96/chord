c153775e32e54c3d56ee492cf2696725175aabf8
ktrip96 <ktrip96@gmail.com>
2023-01-12 10:09:45 +0200

    README improvements

--- back/index.js
+++ back/index.js
@@ -6,11 +6,21 @@ const client_io = require('socket.io-client')
 const sha1 = require('sha1')
 
 const {
-  set_previous, set_next, set_front_socket,
-  get_previous, get_next, get_front_socket,
-  show_neighbours, show_event, hit_node 
+  set_previous,
+  set_next,
+  set_front_socket,
+  get_previous,
+  get_next,
+  get_front_socket,
+  show_neighbours,
+  show_event,
+  hit_node,
 } = require('./globals.js')
-const { join_general_case, join_depart_events, depart } = require('./join_depart.js')
+const {
+  join_general_case,
+  join_depart_events,
+  depart,
+} = require('./join_depart.js')
 const { command_events } = require('./commands.js')
 const { replicate_events, join_replication } = require('./replication.js')
 
@@ -20,7 +30,7 @@ const io = socketio(server, { cors: { origin: '*' } })
 
 const REPLICATION_FACTOR = process.argv[2]
 const ME = process.argv[3]
-const BOOTSTRAP = '192.168.1.71:5000'
+const BOOTSTRAP = 'localhost:5000'
 
 const separator = ME.indexOf(':')
 const MY_PORT = ME.slice(separator + 1)
@@ -34,7 +44,6 @@ if (ME == BOOTSTRAP) {
   set_next(BOOTSTRAP)
 
   io.on('connection', (socket) => {
-
     socket.on('join', ({ joiner }) => {
       // show_event('join', { joiner })
 
@@ -42,12 +51,14 @@ if (ME == BOOTSTRAP) {
       if (get_next() == BOOTSTRAP) {
         // Special case: only bootstrap is in the network, make 2 node network
         hit_node({
-          node: joiner, event_: 'join_response', object: { joiner_previous: BOOTSTRAP, joiner_next: BOOTSTRAP }
+          node: joiner,
+          event_: 'join_response',
+          object: { joiner_previous: BOOTSTRAP, joiner_next: BOOTSTRAP },
         })
-        set_previous(joiner); set_next(joiner)
+        set_previous(joiner)
+        set_next(joiner)
         show_neighbours()
-      } else 
-        join_general_case(joiner, ME)
+      } else join_general_case(joiner, ME)
     })
 
     on_front_connection(socket)
@@ -61,13 +72,13 @@ if (ME == BOOTSTRAP) {
 } else {
   // Non bootstrap code: Begin by asking BOOTSTRAP to join
 
-  hit_node({ node: BOOTSTRAP, event_: 'join', object: { joiner:ME } })
+  hit_node({ node: BOOTSTRAP, event_: 'join', object: { joiner: ME } })
 
   io.on('connection', (socket) => {
-
     socket.on('join_response', ({ joiner_previous, joiner_next }) => {
       // show_event('join_response', { joiner_previous, joiner_next })
-      set_previous(joiner_previous); set_next(joiner_next)
+      set_previous(joiner_previous)
+      set_next(joiner_next)
 
       join_replication(ME)
 
@@ -79,7 +90,10 @@ if (ME == BOOTSTRAP) {
       join_general_case(joiner, ME)
     })
 
-    socket.on('depart', () => { show_event('depart', {}); depart() })
+    socket.on('depart', () => {
+      show_event('depart', {})
+      depart()
+    })
 
     on_front_connection(socket)
 
@@ -95,8 +109,13 @@ function on_front_connection(socket) {
   socket.on('front_connection', () => {
     show_event('front_connection', {})
     set_front_socket(socket)
-    socket.emit('front_connection_response', { previous: get_previous() , next: get_next() })
+    socket.emit('front_connection_response', {
+      previous: get_previous(),
+      next: get_next(),
+    })
   })
 }
 
-server.listen(MY_PORT, () => { console.log(`Server has started on: ${ME}`) })
+server.listen(MY_PORT, () => {
+  console.log(`Server has started on: ${ME}`)
+})
